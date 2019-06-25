a79f42c67f76fc2c02883482314e826138328f1c
RAR5 <RAR5@DELL-PC>
2019-06-25 18:27:02 +0430

    Add project files.

---
+++ HelloGitHub/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace HelloGitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("This is a hello world app for github.");
+            System.Console.Read();
+        }
+    }
+}
